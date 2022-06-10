ce078f52ee571681211f9c492f0417ec09ab0dfd
Bernardo <bdutralemos@gmail.com>
2022-06-09 22:52:40 -0300

    lab10_2 e lab11_3

---
+++ lab11/lab11_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int carater(char *c, char letra){
+    int soma = 0;
+    if(*c == '\0')
+        return 0;
+    if(*c == letra)
+        soma = 1 + carater(c + 1, letra);
+    if(*c != letra)
+        soma = 0 + carater(c + 1, letra);
+    return soma;
+}
+
+int main(){
+    char str[100], letra;
+    int soma;
+    scanf("%s", str);
+    getchar();
+    scanf("%c", &letra);
+    soma = carater(str, letra);
+    printf("%d", soma);
+    return 0;
+}
