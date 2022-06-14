aea89c7e80217467a23be60118e5ed31904667a0
Bernardo <bdutralemos@gmail.com>
2022-06-13 21:42:26 -0300

    iris enhanced

--- lab9/lab9_3.c
+++ lab9/lab9_3.c
@@ -13,10 +13,13 @@ void classificar(Iris nao_identificada, Iris registros_identificados[], int n){
     float menor;
     int pos;
     for (int i = 0; i < n; i++){
-        euclidian[i] += pow(nao_identificada.comS - registros_identificados[i].comS, 2);
-        euclidian[i] += pow(nao_identificada.larS - registros_identificados[i].larS, 2);
+        for (int i = 0; i < 4; i++)
+            euclidian[i] += pow(*(&nao_identificada.comS + i) - *(&registros_identificados[i].comS + i), 2);
+        
+        //euclidian[i] += pow(*(&nao_identificada.comS + i) - *(&registros_identificados[i].comS + i), 2);
+        /*euclidian[i] += pow(nao_identificada.larS - registros_identificados[i].larS, 2);
         euclidian[i] += pow(nao_identificada.comP - registros_identificados[i].comP, 2);
-        euclidian[i] += pow(nao_identificada.larP - registros_identificados[i].larP, 2);
+        euclidian[i] += pow(nao_identificada.larP - registros_identificados[i].larP, 2);*/
         euclidian[i] = sqrt(euclidian[i]);
     }
     menor = euclidian[0];
